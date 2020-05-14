a4d4048e3cda3cbba1891eefcabeb0db4c124db1
Steven Brand <steven.brand1999@gmail.com>
2020-05-13 21:43:19 -0500

    Updates

--- print_stack.c
+++ print_stack.c
@@ -31,7 +31,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		free_prog();
 		exit(EXIT_FAILURE);
 	}
