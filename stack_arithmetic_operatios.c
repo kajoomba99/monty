d6df12215229b0259ba9fedce4a4c43d6a304aad
Steven Brand <steven.brand1999@gmail.com>
2020-05-14 14:48:19 -0500

    Asing value before check if its null in fun add

--- stack_arithmetic_operatios.c
+++ stack_arithmetic_operatios.c
@@ -7,18 +7,19 @@
  */
 void add(stack_t **stack, unsigned int ln)
 {
-	stack_t (*tmp) = (*stack)->next;
+	stack_t *tmp = NULL;
 	int add;
 
-	if (stack == NULL || (*stack)->next == NULL)
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", ln);
 		free_prog();
 		exit(EXIT_FAILURE);
 	}
 
+	tmp = (*stack)->next;
 	add = tmp->n + tmp->prev->n;
-	tmp->n = add;
+	tmp->n = sub;
 	free(tmp->prev);
 	*stack = tmp;
 	tmp->prev = NULL;
